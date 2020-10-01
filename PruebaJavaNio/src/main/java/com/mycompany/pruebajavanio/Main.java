e7f202795fe9024767590110f5719e89823f7ab6
XXnikoDarkXX <nicocheto1@gmail.com>
2020-10-01 22:12:35 +0200

    Ejemplo de Buscar/Crear carpetas

---
+++ PruebaJavaNio/src/main/java/com/mycompany/pruebajavanio/Main.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pruebajavanio;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nicoc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        FileSystem fs = FileSystems.getDefault();//Creamos un File System para poder manejar ficheros
+        System.out.println("abierto? " + fs.isOpen());//para saber si esta abierto el fs
+        System.out.println("Solo lectura? " + fs.isReadOnly());//
+        System.out.println("Separador: " + fs.getSeparator());//
+
+        Path actual = fs.getPath("Imágenes"+fs.getSeparator()+"jpg/test");
+        Path home = fs.getPath("C:\\Users\\nicoc\\");
+        System.out.println("absoluta home: " + home.toAbsolutePath());
+
+        System.out.println("Absoluta actual: " + actual.toAbsolutePath());
+        System.out.println("relativa " +  home.toAbsolutePath().relativize(actual.toAbsolutePath()));//conseguimos la 
+        //ruta relativa para ello debemos usar el home que es el inicio del usuario y usando .relaitive actual le quitamos
+        //con el actual la parte del home C://User//nicoc y dejamos solo lo demás
+        
+        System.out.println("File name: "+actual.getFileName());
+        Iterator it=actual.toAbsolutePath().iterator();
+        System.out.println("Ruta actual despiezada :");
+        while(it.hasNext()){
+            System.out.println((Path)it.next());
+        }
+        
+        System.out.println("Existe actual? "+ Files.exists(actual));
+        try {
+        //    Files.createDirectories(actual);//crea unos directorios sin importar si estan creados o no las carpetas
+            //puede suponer un peligro pues podemos crear carpetas sin saberlo
+            Files.createDirectory(actual);//Con este método si tenemos creadas la carpetas 
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       }
+
+}
